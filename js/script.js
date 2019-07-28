b24cf8f67096f70ee277c877fe62f357b59b3f9f
Etignis <etignis@gmail.com>
2019-07-28 16:40:58 +0300

    wip[window]

--- js/script.js
+++ js/script.js
@@ -166,7 +166,7 @@ window.onload = function(){
     var oTop = "<div class='tabs'><span class='tab selected' data-tab='edit'>"+sTabName+"</span> <span class='tab'  data-tab='history'>Недавние</span> <span class='separator'></span> "+bCross+"</div>"
     var sButton = oData?"<button class='bApplay'>Применить</button>":"<button class='bAdd'>Добавить</button>";
     oHistory = getHistoryList();
-		$("body").append("<div class='mod_win_wrapper' "+nIndex+"><div class='mod_win' id='editor_win'>"+oTop+"<div class='modWinCont' style='max-height: 90%; overflow-y: auto'>"+sForm+oHistory+"</div><div>"+sButton+"</div></div></div>");
+		$("body").append("<div class='mod_win_wrapper' "+nIndex+"><div class='mod_win' id='editor_win'>"+oTop+"<div class='modWinCont' style='max-height: 90%; overflow-y: auto'>"+sForm+oHistory+"</div><div class='footer'>"+sButton+"</div></div></div>");
     $("body").css("overflow-y", "hidden");
     
     if($(".ico_list .selected").length < 1) {
@@ -177,7 +177,14 @@ window.onload = function(){
     }
 		
 		// count icos height
-		//var nGlobal height = $("#editor_win").height();
+		var nGlobalHeight = $("#editor_win").height();
+		var nHeaderHeigth = $("#editor_win .tabs").height();
+		var nFooterHeigth = $("#editor_win .footer").height();
+		
+		var nContHeight = nGlobalHeight - nHeaderHeigth - nFooterHeigth;
+		
+		$("#devlog").text(nGlobalHeight +" "+ nHeaderHeigth +" "+ nFooterHeigth +" "+nContHeight);
+		var nFooterHeigth = $("#editor_win .modWinCont").height(nContHeight+"px");
 		
     
 		$(".mod_win_wrapper").fadeIn();
